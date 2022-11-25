7386016cd696c8da6f130aeeeb1b4fc10681d8d4
emih <emi.programming@gmail.com>
2022-11-25 22:56:35 +0900

    ♻️

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { FirebaseApp, initializeApp } from "firebase/app";
-import { Auth, getAuth,GoogleAuthProvider } from "firebase/auth";
+import { Auth, getAuth, GoogleAuthProvider } from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,20 +9,20 @@ import { Firestore, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
+  appId: "1:281714841625:web:bafb81e111facfed71f288",
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN as string,
+  messagingSenderId: "281714841625",
   projectId: "line-clone2-c341e",
   storageBucket: "line-clone2-c341e.appspot.com",
-  messagingSenderId: "281714841625",
-  appId: "1:281714841625:web:bafb81e111facfed71f288"
 };
 
 // Initialize Firebase
-export const app:FirebaseApp = initializeApp(firebaseConfig);
+export const app: FirebaseApp = initializeApp(firebaseConfig);
 
-export const provider:GoogleAuthProvider = new GoogleAuthProvider();
+export const provider: GoogleAuthProvider = new GoogleAuthProvider();
 
 // ↓引数にappを入れること
-export const auth:Auth = getAuth(app);
+export const auth: Auth = getAuth(app);
 
 // Initialize Realtime Database and get a reference to the service
-export const database:Firestore = getFirestore();
+export const database: Firestore = getFirestore();
--- src/hooks/useSignInWithPopup.ts
+++ src/hooks/useSignInWithPopup.ts
@@ -1,24 +1,27 @@
-import { auth, provider } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
 
-export const useSignInWithPopup =()=>{
-      signInWithPopup(auth, provider)
-      .then((result) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          // const credential = GoogleAuthProvider.credentialFromResult(result);
-          // const token = credential.accessToken;
-          // The signed-in user info.
-          // const user = result.user;
-          // ...
-          alert(`Sign-in successful.`)
-      }).catch((error) => {
-       // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.customData.email;
-        // The AuthCredential type that was used.
-        // const credential:OAuthCredential | null = GoogleAuthProvider.credentialFromError(error);
-        alert(`エラーコード:${errorCode}、エラーメッセージ：${errorMessage}`)
-      });
-}
+import { auth, provider } from "../firebase";
+
+export const useSignInWithPopup = () => {
+  signInWithPopup(auth, provider)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // The signed-in user info.
+      // const user = result.user;
+      // ...
+      alert(`Sign-in successful.`);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      // const email = error.customData.email;
+      // The AuthCredential type that was used.
+      // const credential:OAuthCredential | null = GoogleAuthProvider.credentialFromError(error);
+      alert(`エラーコード:${errorCode}、エラーメッセージ：${errorMessage}`);
+    });
+};
